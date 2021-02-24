24e1d81a5cbd7a57f69c5cca9e2dfef24b598c84
derrekrb <derrek.ray@gmail.com>
2021-02-23 18:18:20 -0700

    Add files via upload
    Project Doc

---
+++ Milestone2ProjectDoc.py
@@ -0,0 +1,85 @@
+"""
+Term Project – Milestone TwoAfter Innovative EDucation (IED) demonstrated the
+ prototype of UVSim to Utah Valley University (UVU), a validation and
+ verification meeting was held involving all the stakeholders.
+ The followings are the meeting outcomes.
+
+ (1) (10 points) The prototype of UVSim meets the basic needs of UVU. UVU
+ will continue the contract with IED. However, UVU concerns the quality of
+ UVSim, and worries about some of the key quality issues may be ignored during
+ the fast prototyping. UVU suggested IED to through away the current prototype
+ and start a new design which can accommodate future functional expansion of
+ UVSim. IED agree to provide a one page justification (single space and 12 pt
+ font size) to justify whether to through away or keep the current prototype.
+
+ (2) (20 points) IED will build Software Requirement Specification (SRS) include
+ Functional Requirement (FR) and Non-Functional Requirement (user friendly)
+ by using Delphi Method. The Delphi process is listed as follow.
+
+ Step 1: Every team member list their own 15 FRs. Discussion is not allowed
+ between team members. There will be 4 separate documents. This is the first
+ iteration, and need to be in the submission.
+
+ Step 2: Every team member send the FR to the team leader, and don't put
+ your name on the document.
+
+ Step 3: Team leader assemble the four documents
+ to two. Send each document to sub-group of two students. Each sub-group
+ discuss, revise, and modify their document.
+
+ Step 4: Once the sub-group
+ agreed upon their FR, submit to the leader. This is the second iteration.
+ Your team should have six (4+2+1) documents now. Again, don't write names
+ on the documents. You need to include these two documents in your submission
+ as well.
+
+ Step 5: Group meeting, assemble the two documents into one. Discuss, modify,
+ and revise to reach the final version. Include the final version in your
+ submission as baseline.
+ Example of functional requirements Requirement
+ Classification The system should allow employees to update their own
+ information, including addresses, phone numbers, emergency contact
+ information, beneficiary information, paycheck deductions, office location,
+ office phone number, and office e-mail address.
+ FunctionalThe system should be accessible over the web and be secure.
+ Non-functionalThe system must provide a searchable online company directory.
+
+
+ (3) (20 points) IED will design Class Diagram as baseline. The Class Diagram
+ should follow the following requirements.
+ • Interfaces, Abstract Classes, and Base Class are allowed.
+ • Apply Model View Controller (MVC) and Façade software design patterns. Use
+ dash line box to circulate the patterns. You need to label the pattern name.
+ • You need to strictly follow “high cohesion and loos coupling” design
+ guideline as follow,
+  No class can has more than 5 coupling with other classes. (hint: coupling
+ types include Content, Common, External, Control, Stamp, and Data couplings)
+  No class can has more than 5 attributes (hint: design data structure
+ if needed).
+  No class can has more than 10 methods. Constructor and accessors are not
+ counted. (hint: cohesion types include Functional, Sequential, Communication,
+ Procedural, Temporal, or Logical cohesion.)
+
+
+ (4) (20 points) UVU require the following increment and enhancement to UVSim
+ simulator. Provide user friendly GUI for UVSim. You will need to submit a
+ wireframe diagram in requirement document. Also, you will need to
+ quantitatively define user friendly in SRS.
+
+
+ (5) (10 points) Provide library documentation for each designed class.
+ The documentation include
+ Description of the purpose of the class
+ Description of each of the arguments
+ Precondition of each of the class
+ Post condition of each of the class
+
+
+ (6) (5 points) Decompose Milestone to backlogs and sprints.
+
+
+ (7) (5 points) Submit Meeting logs and contribution form with signatures.
+
+
+ (8) (10 points) Submit Readme.
+"""
